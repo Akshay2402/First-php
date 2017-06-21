01e43afc1af7b5b4d46812d03e9a1703aba9f869
Akshay Sharma <akshay.sharma2402@gmail.com>
2017-06-21 17:04:03 +0530

    Search based on employe name and department number

---
+++ intelligent_search.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<style>
+form {
+    border: 100px solid #f1f1f1;
+}
+
+input[type=text]{
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+	
+}
+
+button:hover {
+    opacity: 0.8;
+}
+
+
+.container {
+    padding: 20px;
+}
+</style>
+
+<body>
+
+<h2>Know about your Employees </h2>
+
+<form action = "<?php $_PHP_SELF ?>" method = "POST">
+  <div class="container">
+
+    <label><b>Search a employee</b></label>
+    <input type="text" placeholder="Enter employee name / department id" name="name" required>     
+		<button type="submit">Search</button>
+    
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    
+  </div>
+</form>
+
+</body>
+</html>
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "employees";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+$name = $_POST["name"];
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if($name[0] == 'd'){
+		$result = mysqli_query($conn, "SELECT * FROM departments
+    WHERE dept_no LIKE '{$name}' ");
+
+
+		while ($row = mysqli_fetch_array($result))
+		{
+        echo "department name: " . $row['dept_name'] ;
+        echo "<br>";
+		}
+
+}
+else{
+$result = mysqli_query($conn, "SELECT * FROM employees
+    WHERE first_name LIKE '{$name}' or last_name LIKE '{$name}'");
+	
+}
+
+
+while ($row = mysqli_fetch_array($result))
+{
+        echo "First name: " . $row['first_name'] . "&nbsp &nbsp &nbsp  Last name: " . $row['last_name'] . " &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Gender: " . $row['gender'] . "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Emp_id : " . $row['emp_no'] . "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Hire Date: " . $row['hire_date'] ;
+        echo "<br>";
+}
+    mysqli_close($con);
+
+?>
