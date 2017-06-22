68fafead7467cb6089b8b5e8e3b47c2f5ff204ee
vee-pool <vipzshady@gmail.com>
2017-06-22 17:06:45 +0530

    Worked on smart search

--- Search_Page.php
+++ Search_Page.php
@@ -2,7 +2,7 @@
 <html>
 <style>
 form {
-    border: 100px solid #f1f1f1;
+   
 }
 
 input[type=text]{
@@ -12,21 +12,28 @@ input[type=text]{
     display: inline-block;
     border: 1px solid #ccc;
     box-sizing: border-box;
+    box-shadow: 0px 10px 5px #888888;
 }
 
 button {
     background-color: #4CAF50;
     color: white;
-    padding: 14px 20px;
+    padding: 12px 20px;
     margin: 8px 0;
     border: none;
     cursor: pointer;
     width: 100%;
-	
+    font-size: 15px;
+    text-align: center;
+    font-family: sans-serif;
 }
 
+label {
+    cursor: pointer;
+    font-family:  Arial ;
+}
 button:hover {
-    opacity: 0.8;
+    opacity: 1.0;
 }
 
 
@@ -36,19 +43,15 @@ button:hover {
 </style>
 
 <body>
-
-<h2>Know about your Employees </h2>
-
 <form = action = "<?php $_PHP_SELF ?>" method = "GET">
   <div class="container">
-
-    <label><b>Search a employee</b></label>
-    <input type="text" placeholder="Enter employee first name" name="name" required>     
-    <button type="submit">Search</button>
+  <label><b>Eoogle</b></label>
+    <input type="text" placeholder="Search" style="width: 400px; height: 50px" name="name" required>     
+    <button type="submit" style="width: 100px; height: 50px;">Search</button>
     
   </div>
 
-  <div class="container" style="background-color:#f1f1f1">
+  <div class="container" style="background-color:#f2f1f2">
     
   </div>
 </form>
@@ -65,20 +68,119 @@ $dbname = "employees";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 $name = $_GET["name"];
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$result = mysqli_query($conn, "SELECT * FROM employees
-    WHERE first_name LIKE '{$name}' OR last_name LIKE '{$name}'");
+$buff = parseInput($name);
 
-while ($row = mysqli_fetch_array($result))
-{
-        echo "First name: " . $row['first_name'] . "&nbsp &nbsp &nbsp  Last name: " . $row['last_name'] . " &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Gender: " . $row['gender'] . "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Emp_id : " . $row['emp_no'] . "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Hire Date: " . $row['hire_date'] ;
-        echo "<br>";
+switch ($buff) {
+    case 0:
+        # code...
+        findEmployee($conn, $name);
+        break;
+    
+    case 1:
+        # code...
+        findEmployeeByEmpNo($conn, $name);
+        break;
+
+    case 2:
+        # code...
+        findDepartment($conn, $name);
+        break;
 }
+
+function findEmployee($conn, $name)
+{
+    # code...
+    $sql_query = "SELECT * FROM employees
+                    WHERE (first_name = '{$name}' OR last_name = '{$name}') AND 
+                    emp_no IN (
+                    SELECT emp_no
+                    FROM salaries
+                    ORDER BY salary DESC)";
+
+    $result = $conn->query($sql_query);
+
+    echo $result;
+    if ($result->num_rows > 0) {
+        $i = 0;
+        while ($row = $result->fetch_assoc() && $i < 5){
+           echo "First name: " . $row['first_name'] . "&nbsp &nbsp &nbsp  Last name: " . $row['last_name'] . " &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Gender: " . $row['gender'] . "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Emp_id : " . $row['emp_no'] . "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Hire Date: " . $row['hire_date'] ;
+            echo "<br>";
+            $i++;
+        }
+    }
+    else {
+       echo "Error performing query " . $conn->error;
+       
+    }
+    mysqli_close($con);
+}
+
+function findEmployeeByEmpNo($conn, $name)
+{
+    # code...
+    $sql_query = "SELECT * FROM employees
+                    WHERE emp_no = $name";
+    $result = $conn->query($sql_query);
+
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()){
+           echo "First name: " . $row['first_name'] . "&nbsp &nbsp &nbsp  Last name: " . $row['last_name'] . " &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Gender: " . $row['gender'] . "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Emp_id : " . $row['emp_no'] . "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Hire Date: " . $row['hire_date'] ;
+            echo "<br>";
+        }
+    }
+    else {
+       echo "Error performing query " . $conn->error;
+       
+    }
+    mysqli_close($con);
+}
+
+function findDepartment($conn, $name)
+{
+    # code...
+    $sql_query = "SELECT * FROM departments
+                    WHERE dept_no = '{$name}'";
+    $result = $conn->query($sql_query);
+
+    if ($result->num_rows > 0)
+    {
+        while ($row = $result->fetch_assoc()) {
+            # code...
+            echo "its department of ".$row['dept_name'];
+        }
+    }
+    else {
+       echo "Error performing query " . $conn->error;
+       
+    }
     mysqli_close($con);
 
-?>
+}
+function parseInput(string $value)
+{
+    # code...
+    if ( ord($value) < 123 && ord($value) > 96)
+    {
+        if ( $value[1]>123 || $value[1]<96 ) {
+            # code...
+            return 2;
+        }
+    
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+?>                                                          
